f9d1939231c063ce57401ce37aecaa76171db756
Luiz Guilherme De Oliveira Alves <luizinlives@gmail.com>
2026-01-21 19:56:03 -0300

    operadores e condicionais

---
+++ src/operadores.java
@@ -0,0 +1,14 @@
+public class operadores {
+    public static void main(String[] args) throws Exception {
+        int numero1 = 20;
+        int numero2 = 30;
+
+        if(numero1 > numero2){
+            System.out.println("As duas opções são verdadeiras");
+        }else{
+            System.out.println("As duas opções são falsas");
+        }
+   
+       
+    }   
+}
